8887842b6a1cd12a638a212ae1fbbcb5bef1aa7c
Achilleas <Achilleas@tessa>
2013-11-12 18:42:09 +0200

    Added background random spike trains

--- PPDistSlope/PPDistSlope.py
+++ PPDistSlope/PPDistSlope.py
@@ -7,9 +7,10 @@ complexity to see whether the correlation exists on each level.
 
 """
 from brian import *
+import random
 import spike_distance_mp as spikedist
 
-rng = np.random
+nprng = np.random
 dcost = 1000  # cost 1000/second
 
 # First, we'll generate a few pulse packets and calculate the distance on each
@@ -21,26 +22,52 @@ N = 20  # spikes per packet
 packets = []
 intradists = []
 
+print("Calculating distance of single pulse packets without background noise ...")
 samples = 20
 for sigma in sigmas:
     for smpl in range(samples):
         print("sigma: %f, sample: %i" % (sigma, smpl))
         if sigma:
-            newpacket = rng.normal(loc=packet_time, scale=sigma, size=N)
+            newpacket = nprng.normal(loc=packet_time, scale=sigma, size=N)
         else:
             newpacket = ones(N)*packet_time
         newpacket = [[t] for t in newpacket]
         packets.append(newpacket)
         idist = spikedist.mean_pairwise_distance(newpacket, 1000)
         intradists.append((sigma, idist))
-
-print("sigma\tdist")
-for (s, d) in intradists:
-    print("%f\t%f" % (s, d))
-
 sigmas, dists = zip(*intradists)
 scatter(sigmas, dists)
 savefig("singlepacket.png")
+
+print("Adding noise ...")
+intradists = []
+# single spike train - varying rate : equivalent to changing the number
+# of multiple spike trains with fixed rate trains of fixed
+samples = 10
+spikerates = arange(10*Hz, 100*Hz, 10*Hz)
+for bg_rate in spikerates:
+    for sigma in sigmas:
+        for smpl in range(samples):
+            print("bg rate: %f, sigma: %f, sample: %i" % (
+                bg_rate, sigma, smpl))
+            if sigma:
+                newpacket = nprng.normal(loc=packet_time, scale=sigma, size=N)
+            else:
+                newpacket = ones(N)*packet_time
+            lastspike = max(newpacket)
+            newpacket = [[t] for t in newpacket]
+            poissspike = random.expovariate(bg_rate)
+            poissontrain = []
+            while poissspike <= lastspike:
+                poissontrain.append(poissspike)
+                poissspike += random.expovariate(bg_rate)
+            allspikes = newpacket+poissontrain  # must always be sure they are lists
+            idist = spikedist.mean_pairwise_distance(allspikes, 1000)
+            intradists.append((bg_rate, sigma, idist))
+rates, sigmas, dists = zip(*intradists)
+scatter(sigmas, dists, c=rates)
+colorbar()
+savefig("singlepacket_noise.png")
 
 """
 plot and save
