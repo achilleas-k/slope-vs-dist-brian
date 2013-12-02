019a70cb324b8ae942f129897fbfcaa607e01222
Achilleas <Achilleas@tessa>
2013-12-02 20:59:20 +0200

    Adding multiple packets

--- PPDistSlope/PPDistSlope.py
+++ PPDistSlope/PPDistSlope.py
@@ -23,6 +23,9 @@ def arrays_to_spiketimes(thearrays):
                 spiketimes.append((i, spike))
         return spiketimes
 
+defaultclock.dt = dt = 0.1*ms
+duration = 2*second
+
 warnings.simplefilter('always')
 nprng = np.random
 slope_w = 0.5*ms
@@ -30,88 +33,77 @@ slope_w = 0.5*ms
 # and equal 2 when moving across a time w
 dcost = 2/slope_w
 
-# First, we'll generate a few pulse packets and calculate the distance on each
-# one individually
 
-packet_time = 0.5*second  # reduce the chance of negative spike times
+packet_times = [0.5*second, 1.5*second]
 N = 20  # spikes per packet
 packets = []
 sigmas = frange(0*ms, 4*ms, 0.1*ms)
-samples = 1
 randtrains = [5, 10, 15, 20, 25, 30]
 spikerates = frange(10*Hz, 100*Hz, 10*Hz)
 
-print("Calculating distance of single pulse packets with background noise ...")
 results = []
 for bg_rate in spikerates:
     interm_results = []
     for nrand in randtrains:
         for sigma in sigmas:
-            for smpl in range(samples):
+            packets = []
+            for ptime in packet_times:
                 defaultclock.reinit()
                 clear(True, True)
                 gc.collect()
-                # need to re-define stop condition after deleting
-                # everything
-                @network_operation
-                def stop_condition(clock):
-                    if spikemon.nspikes:
-                        stop()
-
                 if sigma:
-                    newpacket = nprng.normal(loc=packet_time, scale=sigma, size=N)
+                    newpacket = nprng.normal(loc=ptime, scale=sigma, size=N)
                 else:
-                    newpacket = ones(N)*packet_time
-                lastspike = max(newpacket)
+                    newpacket = ones(N)*ptime
                 newpacket = [[t] for t in newpacket]
-                randspiketrains = []
-                for nr in range(nrand):
-                    poissontrain = []
-                    poissspike = random.expovariate(bg_rate)
-                    while poissspike <= 1:  # 1 second duration
-                        poissontrain.append(poissspike)
-                        poissspike += random.expovariate(bg_rate)
-                    randspiketrains.append(poissontrain)
-                allspikes = newpacket+randspiketrains  # must always be sure they are lists
-                spiketimes = arrays_to_spiketimes(allspikes)
-                if not len(allspikes) == nrand + N:
-                    warningsd.warn("Whaaaat?! Something fucked up. Check spiketrains.")
-                weight = 1.5*15*mV/len(allspikes)
-                inputgroup = SpikeGeneratorGroup(len(allspikes), spiketimes)
-                # TODO: no reset - no clipping - Jacob's suggestion
-                neuron = NeuronGroup(1, 'dV/dt = -V/(10*ms) : volt',
-                        threshold='V>15*mV', reset='V=0*mV', refractory=2*ms)
-                conn = Connection(source=inputgroup, target=neuron,
-                        state='V', weight=weight)
-                neuron.V = 0*mV
-                statemon = StateMonitor(neuron, 'V', record=True)
-                spikemon = SpikeMonitor(neuron, record=True)
-                netw = Network(inputgroup, neuron, conn, statemon,
-                        spikemon, stop_condition)
-                netw.run(1.5*second)
-                statemon.insert_spikes(spikemon, 15*mV)
-                if len(spikemon[0]) > 1:
-                    warnings.warn("More than one spike fired.")
-                print("nrand: %i, bg rate: %f, sigma: %f, sample: %i" % (
-                    nrand, bg_rate, sigma, smpl))
-                if len(spikemon[0]):
-                    idist = mean_pairwise_distance(allspikes, 1000)
-                    for outspike in spikemon[0]:
-                        outspike *= second
-                        spike_dt = outspike/(0.1*ms)
-                        w_start = outspike-slope_w
-                        w_start_dt = w_start/(0.1*ms)
-                        v_w_start = statemon[0][w_start_dt]
-                        slope = (15*mV-v_w_start*volt)/slope_w
-                        interm_results.append((nrand, bg_rate, sigma, idist, slope))
-                        results.append((nrand, bg_rate, sigma, idist, slope))
-                        print("\t\tdist: %f, slope: %f" % (idist, slope))
-                else:
-                    print("\t\tNo spike fired.")
-                if slope < 0:
-                    warnings.warn("Negative slope - this is a problem!")
-                # clear everything related to brian
-                del(inputgroup, neuron, conn, statemon, spikemon, netw)
+                packets += newpacket
+            randspiketrains = []
+            for nr in range(nrand):
+                poissontrain = []
+                poissspike = random.expovariate(bg_rate)
+                while poissspike <= int(duration):
+                    poissontrain.append(poissspike)
+                    poissspike += random.expovariate(bg_rate)
+                randspiketrains.append(poissontrain)
+            allspikes = packets+randspiketrains  # must always be sure they are lists
+            spiketimes = arrays_to_spiketimes(allspikes)
+            if not len(allspikes) == nrand + N:
+                warnings.warn("Something went horribly wrong. Check spiketrains.")
+            weight = 1.5*15*mV/len(allspikes)
+            inputgroup = SpikeGeneratorGroup(len(allspikes), spiketimes)
+            # TODO: no reset - no clipping - Jacob's suggestion
+            neuron = NeuronGroup(1, 'dV/dt = -V/(10*ms) : volt',
+                    threshold='V>15*mV', reset='V=0*mV', refractory=2*ms)
+            conn = Connection(source=inputgroup, target=neuron,
+                    state='V', weight=weight)
+            neuron.V = 0*mV
+            statemon = StateMonitor(neuron, 'V', record=True)
+            spikemon = SpikeMonitor(neuron, record=True)
+            netw = Network(inputgroup, neuron, conn, statemon, spikemon)
+            netw.run(2*second)
+            statemon.insert_spikes(spikemon, 15*mV)
+            if len(spikemon[0]) > 2:
+                warnings.warn("More than one spike fired.")
+            print("nrand: %i, bg rate: %f, sigma: %f, sample: %i" % (
+                nrand, bg_rate, sigma, smpl))
+            if len(spikemon[0]):
+                idist = mean_pairwise_distance(allspikes, 1000)
+                for outspike in spikemon[0]:
+                    outspike *= second
+                    spike_dt = outspike/(0.1*ms)
+                    w_start = outspike-slope_w
+                    w_start_dt = w_start/(0.1*ms)
+                    v_w_start = statemon[0][w_start_dt]
+                    slope = (15*mV-v_w_start*volt)/slope_w
+                    interm_results.append((nrand, bg_rate, sigma, idist, slope))
+                    results.append((nrand, bg_rate, sigma, idist, slope))
+                    print("\t\tdist: %f, slope: %f" % (idist, slope))
+            else:
+                print("\t\tNo spike fired.")
+            if slope < 0:
+                warnings.warn("Negative slope - this is a problem!")
+            # clear everything related to brian
+            del(inputgroup, neuron, conn, statemon, spikemon, netw)
 
     rands, rates, sigs, dists, slopes = zip(*interm_results)
     scatter(slopes, dists, c=sigs)
