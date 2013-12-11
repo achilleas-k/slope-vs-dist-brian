33463482b6d9cbd9e7ac4c57a09dba9cf24447bf
Achilleas <Achilleas@tessa>
2013-12-11 20:57:45 +0200

    Turning into multineuron sim with results

--- TimeResolvedComparison.py
+++ TimeResolvedComparison.py
@@ -1,52 +1,136 @@
 from brian import *
 import warnings
 from spike_distance_mp import mean_pairwise_distance
-from neurotools import npss, genInputGroups
+from neurotools import norm_firing_slope, genInputGroups, npss
+from spike_distance_kreuz import multivariate_spike_distance
 
 
-def tuples_to_spiketrains(tuples):
-    spiketrains = {}
-    for (i, t) in tuples:
-        try:
-            spiketrains[i].append(t)
-        except KeyError:
-            spiketrains[i] = [t]
-    return spiketrains
+def interval_VP(inputspikes, outputspikes, cost, dt=0.1*ms):
+    dt = float(dt)
+    vpdists = []
+    for prv, nxt in zip(outputspikes[:-1], outputspikes[1:]):
+        interval_inputs = []
+        for insp in inputspikes:
+            interval_inputs.append(insp[(prv < insp) & (insp < nxt+dt)])
+        vpd = mean_pairwise_distance(interval_inputs, cost)
+        vpdists.append(vpd)
+    return vpdists
+
+
+def interval_Kr(inputspikes, outputspikes, dt=0.1*ms):
+    dt = float(dt)
+    krdists = []
+    for prv, nxt in zip(outputspikes[:-1], outputspikes[1:]):
+        krd = multivariate_spike_distance(inputspikes, prv, nxt+dt, 1)
+        krdists.append(krd[1])
+    return krdists
 
 
 defaultclock.dt = dt = 0.1*ms
-duration = 1*second
+duration = 5*second
 
 warnings.simplefilter("always")
 slope_w = 2*msecond
 dcost = float(2/slope_w)
 
 N_in = 100
-f_in = 50*Hz
+f_in = 30*Hz
+S_in = frange(0, 1, 0.1)
 
 # neuron
-neuron = NeuronGroup(1, "dV/dt = -V/(10*ms) : volt",
+neuron = NeuronGroup(len(S_in), "dV/dt = -V/(10*ms) : volt",
         threshold="V>15*mvolt", reset="V=0*mvolt")
 neuron.V = 0*mvolt
+netw = Network(neuron)
 
-# inputs
-syncInp, randInp = genInputGroups(N_in, f_in, 0.5, 0*ms, duration, dt)
-syncCon = Connection(syncInp, neuron, "V", weight=0.2*mV)
-randCon = Connection(randInp, neuron, "V", weight=0.2*mV)
+syncGens = []
+randGens = []
+syncCons = []
+randCons = []
+syncMons = []
+randMons = []
+for idx, sync in enumerate(S_in):
+    # inputs
+    syncInp, randInp = genInputGroups(N_in, f_in, synx, 0*ms, duration, dt)
+    syncGens.append(syncInp)
+    randGens.append(randInp)
+    netw.add(syncInp, randInp)
+    # connections
+    syncCon = Connection(syncInp, neuron, "V", weight=0.3*mV)
+    randCon = Connection(randInp, neuron, "V", weight=0.3*mV)
+    syncCons.append(syncCon)
+    randCons.append(randCon)
+    netw.add(syncCon, randCon)
+    # monitors
+    syncmon = SpikeMonitor(syncInp)
+    randmon = SpikeMonitor(randInp)
+    syncMons.append(syncmon)
+    randMons.append(randmon)
+    netw.add(syncmon, randmon)
 
-# monitors
-syncmon = SpikeMonitor(syncInp)
-randmon = SpikeMonitor(randInp)
+# global monitors
 vmon = StateMonitor(neuron, "V", record=True)
 outmon = SpikeMonitor(neuron)
-netw = Network(neuron, vmon, outmon, syncInp, randInp, syncCon, randCon,
-        syncmon, randmon)
+netw = Network(vmon, outmon)
 # run
+print("Running ...")
 netw.run(duration, report="stdout")
 
-# TODO: Collect input spikes, calculate npss, calculate mean pairwise distance
-# between input spikes for:
-# (a) 10 ms (leak time constant) prior to firing
-# (b) entire inter-spike interval
-# Do for both V-P and Kreuz
+if len(outmon[0]) == 0:
+    print("No spikes fired. Aborting!")
+    sys.exit(0)
+
+# TODO: correlation coefficient on binned spike trains.
+
+# collect input spikes into array of arrays
+print("Collecting input spikes ...")
+inputSpikeTrainCollection = []
+for syncmon, randmon in zip(syncMons, randMons):
+    input_spiketrains = syncmon.spiketimes.values()
+    input_spiketrains += randmon.spiketimes.values()
+    input_spiketrains = array(input_spiketrains)
+    inputSpikeTrainCollection.append(input_spiketrains)
+
+BREAK HERE
+CONTINUE WITH MULTI NEURON SIMULATION - ONE PER S_IN
+# calculate npss
+print("Calculating slope measure ...")
+mslope, slopes = norm_firing_slope(vmon[0], outmon[0], 15*mV, 2*ms, dt)
+_, otherslopes = npss(vmon[0], outmon[0], 15*mV, 2*ms, dt)
+
+# calculate mean pairwise V-P distance for each interval
+print("Calculating VP distance ...")
+vp_dists = interval_VP(input_spiketrains, outmon[0], dcost)
+
+# calculate multivariate Kreuz spike distance
+print("Calculating Kreuz distance ...")
+kr_dists = interval_Kr(input_spiketrains, outmon[0])
+
+print("Plotting ...")
+subplot2grid((5,1), (0,0), rowspan=1, colspan=1)
+plot(outmon[0][1:], slopes)
+axis(xmin=0, xmax=float(duration))
+
+subplot2grid((5,1), (1,0), rowspan=1, colspan=1)
+plot(outmon[0][1:], otherslopes)
+axis(xmin=0, xmax=float(duration))
+
+subplot2grid((5,1), (2,0), rowspan=1, colspan=1)
+plot(outmon[0][1:], vp_dists)
+axis(xmin=0, xmax=float(duration))
+
+subplot2grid((5,1), (3,0), rowspan=1, colspan=1)
+plot(outmon[0][1:], kr_dists)
+axis(xmin=0, xmax=float(duration))
+
+subplot2grid((5,1), (4,0), rowspan=1, colspan=1)
+raster_plot(syncmon, randmon)
+xmin, xmax, ymin, ymax = axis()
+for ospk in outmon[0]:
+    plot([ospk*1000, ospk*1000], [ymin, ymax], 'r-')
+axis(xmin=0, xmax=float(duration)*1000)
+
+ion()
+show()
+
 
