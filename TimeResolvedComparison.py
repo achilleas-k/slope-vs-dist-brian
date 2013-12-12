95e145756d80c06b1e9a204369b06e69bd91fc75
Achilleas <Achilleas@tessa>
2013-12-12 05:06:20 +0200

    Multiple simulations - doesn't save data

--- TimeResolvedComparison.py
+++ TimeResolvedComparison.py
@@ -36,9 +36,10 @@ dcost = float(2/slope_w)
 N_in = 100
 f_in = 30*Hz
 S_in = frange(0, 1, 0.1)
+Nsims = len(S_in)
 
 # neuron
-neuron = NeuronGroup(len(S_in), "dV/dt = -V/(10*ms) : volt",
+neuron = NeuronGroup(Nsims, "dV/dt = -V/(10*ms) : volt",
         threshold="V>15*mvolt", reset="V=0*mvolt")
 neuron.V = 0*mvolt
 netw = Network(neuron)
@@ -51,13 +52,13 @@ syncMons = []
 randMons = []
 for idx, sync in enumerate(S_in):
     # inputs
-    syncInp, randInp = genInputGroups(N_in, f_in, synx, 0*ms, duration, dt)
+    syncInp, randInp = genInputGroups(N_in, f_in, sync, 0*ms, duration, dt)
     syncGens.append(syncInp)
     randGens.append(randInp)
     netw.add(syncInp, randInp)
     # connections
-    syncCon = Connection(syncInp, neuron, "V", weight=0.3*mV)
-    randCon = Connection(randInp, neuron, "V", weight=0.3*mV)
+    syncCon = Connection(syncInp, neuron[idx], "V", weight=0.3*mV)
+    randCon = Connection(randInp, neuron[idx], "V", weight=0.3*mV)
     syncCons.append(syncCon)
     randCons.append(randCon)
     netw.add(syncCon, randCon)
@@ -71,12 +72,12 @@ for idx, sync in enumerate(S_in):
 # global monitors
 vmon = StateMonitor(neuron, "V", record=True)
 outmon = SpikeMonitor(neuron)
-netw = Network(vmon, outmon)
+netw.add(vmon, outmon)
 # run
 print("Running ...")
 netw.run(duration, report="stdout")
 
-if len(outmon[0]) == 0:
+if outmon.nspikes == 0:
     print("No spikes fired. Aborting!")
     sys.exit(0)
 
@@ -84,27 +85,38 @@ if len(outmon[0]) == 0:
 
 # collect input spikes into array of arrays
 print("Collecting input spikes ...")
-inputSpikeTrainCollection = []
+input_spiketrain_collection = []
 for syncmon, randmon in zip(syncMons, randMons):
     input_spiketrains = syncmon.spiketimes.values()
     input_spiketrains += randmon.spiketimes.values()
     input_spiketrains = array(input_spiketrains)
-    inputSpikeTrainCollection.append(input_spiketrains)
+    input_spiketrain_collection.append(input_spiketrains)
 
-BREAK HERE
-CONTINUE WITH MULTI NEURON SIMULATION - ONE PER S_IN
-# calculate npss
-print("Calculating slope measure ...")
-mslope, slopes = norm_firing_slope(vmon[0], outmon[0], 15*mV, 2*ms, dt)
-_, otherslopes = npss(vmon[0], outmon[0], 15*mV, 2*ms, dt)
+mslope_collection = []
+slopes_collection = []
+oldslopes_collection = []
+vp_dist_collection = []
+kr_dist_collection = []
+for idx in range(Nsims):
+    # calculate npss
+    print("%i: Calculating slope measure ..." % idx)
+    mslope, slopes = norm_firing_slope(vmon[idx], outmon[idx],
+            15*mV, 2*ms, dt)
+    _, oldslopes = npss(vmon[idx], outmon[idx], 15*mV, 2*ms, dt)
+    mslope_collection.append(mslope)
+    slopes_collection.append(slopes)
+    oldslopes_collection.append(oldslopes)
 
-# calculate mean pairwise V-P distance for each interval
-print("Calculating VP distance ...")
-vp_dists = interval_VP(input_spiketrains, outmon[0], dcost)
+    input_spiketrains = input_spiketrain_collection[idx]
+    # calculate mean pairwise V-P distance for each interval
+    print("%i: Calculating VP distance ..." % idx)
+    vp_dists = interval_VP(input_spiketrains, outmon[idx], dcost)
+    vp_dist_collection.append(vp_dists)
 
-# calculate multivariate Kreuz spike distance
-print("Calculating Kreuz distance ...")
-kr_dists = interval_Kr(input_spiketrains, outmon[0])
+    # calculate multivariate Kreuz spike distance
+    print("%i: Calculating Kreuz distance ..." % idx)
+    kr_dists = interval_Kr(input_spiketrains, outmon[idx])
+    kr_dist_collection.append(kr_dists)
 
 print("Plotting ...")
 subplot2grid((5,1), (0,0), rowspan=1, colspan=1)
@@ -112,7 +124,7 @@ plot(outmon[0][1:], slopes)
 axis(xmin=0, xmax=float(duration))
 
 subplot2grid((5,1), (1,0), rowspan=1, colspan=1)
-plot(outmon[0][1:], otherslopes)
+plot(outmon[0][1:], oldslopes)
 axis(xmin=0, xmax=float(duration))
 
 subplot2grid((5,1), (2,0), rowspan=1, colspan=1)
