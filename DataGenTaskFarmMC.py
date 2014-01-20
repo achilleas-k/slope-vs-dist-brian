d5dff17e56318f19ab0d67e3b1cfc876283d3231
Achilleas <Achilleas@tessa>
2014-01-20 18:23:49 +0200

    Minor bug

--- DataGenTaskFarmMC.py
+++ DataGenTaskFarmMC.py
@@ -52,13 +52,10 @@ def interval_corr(inputspikes, outputspikes, b=0.1*ms, duration=None):
     for prv, nxt in zip(outputspikes[:-1], outputspikes[1:]):
         interval_inputs = []
         for insp in inputspikes:
-            interval_spikes = insp[(prv < insp) & (insp < nxt+b)]-prv
+            interval_spikes = insp[(prv < insp) & (insp <= nxt)]-prv
             if len(interval_spikes):
                 interval_inputs.append(interval_spikes)
-        if len(interval_inputs):
-            corrs_i = mean(corrcoef_spiketrains(interval_inputs, b, duration))
-        else:
-            corrs.append(0)
+        corrs_i = mean(corrcoef_spiketrains(interval_inputs, b, duration))
         corrs.append(corrs_i)
     return corrs
 
