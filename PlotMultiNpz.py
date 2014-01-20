c5909b5a50b943e66557f56d6910e6ae74cdc460
Achilleas <Achilleas@tessa>
2014-01-20 18:25:29 +0200

    Pickle data dump and calculate modulus metric

--- PlotMultiNpz.py
+++ PlotMultiNpz.py
@@ -2,22 +2,26 @@ from __future__ import print_function
 from brian import *
 import os
 import sys
+import pickle
+from interval_modulus_metric import interval_modm
 
-datadir = sys.argv[1]
+directories = sys.argv[1:]
 
-filesindir = os.listdir(datadir)
-npzindir = [fid for fid in filesindir if fid.endswith("npz")]
-npzcount = len(npzindir)
 results = []
 configs = []
-for idx, npzfile in enumerate(npzindir):
-    npzdata = load(os.path.join(datadir, npzfile))
-    res = npzdata["results"].item()
-    conf = npzdata["config"].item()
-    results.append(res)
-    configs.append(conf)
-    print("Finished reading %s (%i/%i)" % (npzfile, idx+1, npzcount), end="\r")
-print("")
+for datadir in directories:
+    print("Loading files from %s ..." % datadir)
+    filesindir = os.listdir(datadir)
+    npzindir = [fid for fid in filesindir if fid.endswith("npz")]
+    npzcount = len(npzindir)
+    for idx, npzfile in enumerate(npzindir):
+        npzdata = load(os.path.join(datadir, npzfile))
+        res = npzdata["results"].item()
+        conf = npzdata["config"].item()
+        results.append(res)
+        configs.append(conf)
+        print("Finished reading %s (%i/%i)" % (npzfile, idx+1, npzcount), end="\r")
+    print("")
 
 # recalculate correlation coefficient with bin=w (takes ALL THE AGES)
 #print("Recalculating correlation coefficients with bin=w ...")
@@ -38,62 +42,93 @@ print("Reorganising data into arrays ...")
 # organise data into arrays
 # old implementations of npss can cause NaNs, so let's get rid of them
 npss = [res["npss"] for res in results]
-_idx = [-isnan(n) for n in npss]
-npss = [n[i] for i, n in zip(_idx, npss)]
+notnan = [flatnonzero(-isnan(n)) for n in npss]
+npss = [n[i] for i, n in zip(notnan, npss)]
 npss_mean = [mean(n) for n in npss]
 
 corr = [array(res["correlations"]) for res in results]
-corr = [c[i] for i, c in zip(_idx, corr)]
+corr = [c[i] for i, c in zip(notnan, corr)]
 corr_mean = [mean(c) for c in corr]
 
 vp_d = [array(res["vp_dists"]) for res in results]
-vp_d = [v[i] for i, v in zip(_idx, vp_d)]
+vp_d = [v[i] for i, v in zip(notnan, vp_d)]
 vp_d_mean = [mean(v) for v in vp_d]
 
 kr_d = [array(res["kr_dists"]) for res in results]
-kr_d = [k[i] for i, k in zip(_idx, kr_d)]
+kr_d = [k[i] for i, k in zip(notnan, kr_d)]
 kr_d_mean = [mean(k) for k in kr_d]
+
+print("Calculating mean interval modulus metric ...")
+mm_d = []
+mm_d_mean = []
+for idx, (res, notnan_idx) in enumerate(zip(results, notnan)):
+    inspikes = res["inspikes"]
+    outspikes = res["outspikes"]
+    mm_d_i = interval_modm(inspikes, outspikes, 0, 5)
+    try:
+        mm_d_i = [mm_d_i[i] for i in notnan_idx]  # remove the NaN indices
+    except IndexError:
+        import IPython; IPython.embed()
+    mm_d.append(mean(mm_d_i))
+    mm_d_mean.append(mean(mm_d))
+    print("Finished calculating %i/%i" % (idx+1, len(results)), end="\r")
+    sys.stdout.flush()
+os.mkdir("plots")
+
+allresults = {"npss": npss, "npss_mean": npss_mean,
+              "corr": corr, "corr_mean": corr_mean,
+              "vp_d": vp_d, "vp_d_mean": vp_d_mean,
+              "kr_d": kr_d, "kr_d_mean": kr_d_mean,
+              "mm_d": mm_d, "mm_d_mean": mm_d_mean,
+             }
+pickle.dump(allresults, open("metric_comp_results.pkl", "w"))
+print("Results and averages saved to metric_comp_results.pkl")
+
 
 # plot means
 scatter(npss_mean, corr_mean)
 xlabel("NPSS"); ylabel("Correlation coefficient")
-savefig("npss_corr.eps")
+savefig("plots/npss_corr.eps")
 print("Plotted npss_corr.eps")
 clf()
 scatter(npss_mean, vp_d_mean)
 xlabel("NPSS"); ylabel("V-P")
-savefig("npss_vp.eps")
+savefig("plots/npss_vp.eps")
 print("Plotted npss_vp.eps")
 clf()
 scatter(npss_mean, kr_d_mean)
 xlabel("NPSS"); ylabel("Kreuz")
-savefig("npss_kr.eps")
+savefig("plots/npss_kr.eps")
 print("Plotted npss_kr.eps")
+clf()
+scatter(npss_mean, mm_d_mean)
+savefig("plots/npps_modm.eps")
+print("Plotted npss_modm.eps")
 clf()
 
 # plot individual points
-hold(True)
-for n, c, v, k in zip(npss, corr, vp_d, kr_d):
-    figure(1)
-    scatter(n, c, c='k')
-    figure(2)
-    scatter(n, v, c='k')
-    figure(3)
-    scatter(n, k, c='k')
-
-f = figure(1)
-xlabel("NPSS"); ylabel("Correlation coefficient")
-f.savefig("npss_corr_ind.eps")
-print("Plotted npss_corr_ind.eps")
-f.savefig("npss_corr_ind.eps")
-f = figure(2)
-xlabel("NPSS"); ylabel("V-P")
-f.savefig("npss_vp_ind.eps")
-print("Plotted npss_vp_ind.eps")
-f = figure(3)
-xlabel("NPSS"); ylabel("Kreuz")
-f.savefig("npss_kr_ind.eps")
-print("Plotted npss_kr_ind.eps")
+#hold(True)
+#for n, c, v, k in zip(npss, corr, vp_d, kr_d):
+#    figure(1)
+#    scatter(n, c, c='k')
+#    figure(2)
+#    scatter(n, v, c='k')
+#    figure(3)
+#    scatter(n, k, c='k')
+#
+#f = figure(1)
+#xlabel("NPSS"); ylabel("Correlation coefficient")
+#f.savefig("plots/npss_corr_ind.eps")
+#print("Plotted npss_corr_ind.eps")
+#f.savefig("plots/npss_corr_ind.eps")
+#f = figure(2)
+#xlabel("NPSS"); ylabel("V-P")
+#f.savefig("plots/npss_vp_ind.eps")
+#print("Plotted npss_vp_ind.eps")
+#f = figure(3)
+#xlabel("NPSS"); ylabel("Kreuz")
+#f.savefig("plots/npss_kr_ind.eps")
+#print("Plotted npss_kr_ind.eps")
 
 print("Correlation between means:")
 print("\tNPSS\tCorr\tV-P\tKreuz")
