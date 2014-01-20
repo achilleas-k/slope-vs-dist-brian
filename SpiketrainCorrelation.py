6b2734542e67642610fac54a00976dc9c685a629
Achilleas <Achilleas@tessa>
2014-01-20 18:25:15 +0200

    Minor window bug

--- SpiketrainCorrelation.py
+++ SpiketrainCorrelation.py
@@ -7,14 +7,14 @@ def interval_corr(inputspikes, outputspikes, b=0.1*ms, duration=None):
     for prv, nxt in zip(outputspikes[:-1], outputspikes[1:]):
         interval_inputs = []
         for insp in inputspikes:
-            interval_spikes = insp[(prv < insp) & (insp < nxt+b)]-prv
+            interval_spikes = insp[(prv < insp) & (insp <= nxt)]-prv
             if len(interval_spikes):
                 interval_inputs.append(interval_spikes)
         if len(interval_inputs):
             corrs_i = mean(nt.corrcoef_spiketrains(interval_inputs, b, duration))
+            corrs.append(corrs_i)
         else:
             corrs.append(0)
-        corrs.append(corrs_i)
     return corrs
 
 
