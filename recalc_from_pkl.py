2553c0750e225b4f4d31c64132d1be02458c66b9
Achilleas <Achilleas@tessa>
2014-01-30 20:30:34 +0200

    Typos and slice range

--- recalc_from_pkl.py
+++ recalc_from_pkl.py
@@ -17,7 +17,7 @@ def calc_metrics(conres):
     mem = result["mem"]
     npss = result["npss"]  # trusting this one - the rest will be recalced
     vp_dists = metrics.victor_purpura.interval(inspikes, outspikes,
-                                               cost=1.0/0.002, mp=True)
+                                               cost=1.0/0.002, mp=False)
     kr_dists = metrics.kreuz.interval(inspikes, outspikes)
     corrs = metrics.corrcoef.interval_corr(inspikes, outspikes, 0.002)
     return {"id": _id,
@@ -29,20 +29,23 @@ def calc_metrics(conres):
 
 
 pickle_file = sys.argv[1]
+print("Loading data from pkl file %s ..." % (pickle_file))
 data = pickle.load(open(pickle_file))
 results = data["results"]
 config = data["config"]
 
 _numitems = len(results)
+print("Found %i data items ..." % _numitems)
 
 pool = mp.Pool()
-metric_calc_iter = pool.imap(calc_metrics, zip(config[:3], results[:3]))
+metric_calc_iter = pool.imap(calc_metrics, zip(config, results))
 recalculated_metrics = []
+print("Starting recalculation of metrics ...")
 for idx, recmet in enumerate(metric_calc_iter, 1):
     recalculated_metrics.append(recmet)
     print("\r%i/%i ..." % (idx, _numitems))
     sys.stdout.flush()
-print("Calcultion complete")
+print("Calculation complete")
 
 # just dump for now - I need to get home
 pickle.dump(recalculated_metrics, open("recalculated_metrics.pkl", "w"))
