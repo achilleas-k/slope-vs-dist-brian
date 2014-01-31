d393b5df011907e68059170c6bd02e34a649ba42
Achilleas <Achilleas@Morgana>
2014-02-01 01:51:49 +0200

    New script: Calculate the binned corr on the grid

---
+++ grid_correlation.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append("./spikerlib.egg")
+from spikerlib.metrics import corrcoef as cc
+import numpy as np
+import multiprocessing as mp
+
+
+def call_interval_cc(record):
+    id = record["id"]
+    inspikes = record["inspikes"]
+    outspikes = record["outspikes"]
+    corrcoef = np.mean(cc.interval(inspikes, outspikes, b=0.002, duration=5))
+    return {"id": id, "corr": corrcoef}
+
+spikes = np.load("2014-01-31_spikes.npz")["data"]
+
+
+pool = mp.Pool()
+corrcoef = pool.map(call_interval_cc, spikes)
+np.savez("victor_purpura.npz", data=corrcoef)
+
+
