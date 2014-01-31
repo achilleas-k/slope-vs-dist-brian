4abbf03ced24cf942a39ab07e98a3d7cb00ae828
Achilleas <Achilleas@Morgana>
2014-01-31 17:39:42 +0200

    Guessing duration for interval_corr

--- recalc_from_pkl.py
+++ recalc_from_pkl.py
@@ -4,7 +4,6 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import spikerlib.metrics as metrics
-from spikerlib.tools import normalised_pre_spike_slopes
 import multiprocessing as mp
 
 
@@ -18,12 +17,13 @@ def calc_metrics(conres):
     npss = result["npss"]  # trusting this one - the rest will be recalced
     vp_dists = metrics.victor_purpura.interval(inspikes, outspikes,
                                                cost=1.0/0.002, mp=False)
-    kr_dists = metrics.kreuz.interval(inspikes, outspikes)
+    #kr_dists = metrics.kreuz.interval(inspikes, outspikes)
     corrs = metrics.corrcoef.interval_corr(inspikes, outspikes, 0.002)
     return {"id": _id,
-            "vp_dists": vp_dists,
-            "kr_dists": kr_dists,
-            "correlations": corrs}
+    #        "vp_dists": vp_dists,
+    #        "kr_dists": kr_dists,
+    #        "correlations": corrs,
+           }
 
 
 
@@ -38,12 +38,12 @@ _numitems = len(results)
 print("Found %i data items ..." % _numitems)
 
 pool = mp.Pool()
-metric_calc_iter = pool.imap(calc_metrics, zip(config, results))
+metric_calc_iter = pool.imap(calc_metrics, zip(config[:4], results[:4]))
 recalculated_metrics = []
 print("Starting recalculation of metrics ...")
 for idx, recmet in enumerate(metric_calc_iter, 1):
     recalculated_metrics.append(recmet)
-    print("\r%i/%i ..." % (idx, _numitems))
+    print("\r%i/%i ..." % (idx, _numitems), end="\r")
     sys.stdout.flush()
 print("Calculation complete")
 
