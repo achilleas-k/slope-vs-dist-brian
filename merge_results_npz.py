ca36c3d2f31c32ecfe7827632cf6fe9aa0d91794
Achilleas <Achilleas@Morgana>
2014-02-05 20:45:59 +0200

    Merge measures from different npz files into one
    Individual npz files result from grid calculation

---
+++ merge_results_npz.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import sys
+import os
+import numpy as np
+
+directory = sys.argv[1]
+
+npss_dict = np.load(os.path.join(directory, "npss.npz"))["data"]
+modulus_dict = np.load(os.path.join(directory, "modulus.npz"))["data"]
+vp_dict = np.load(os.path.join(directory, "victor_purpura.npz"))["data"]
+
+measures = {}
+ids = [_n["id"] for _n in npss_dict]
+for idx, _id in enumerate(ids):
+    # find matching npss
+    npss = [_n["npss"] for _n in npss_dict if _n["id"] == _id]
+    # find matching vp
+    vp = [_v["vp"] for _v in vp_dict if _v["id"] == _id]
+    # find matching modulus
+    mod = [_m["modulus"] for _m in modulus_dict if _m["id"] == _id]
+    metrics_dict = {"npss": npss, "vp": vp, "modulus": mod}
+    measures[str(_id)] = metrics_dict
+    print("%i/%i" % (idx, len(ids)), end="\r")
+
+output_filename = os.path.join("measures.npz")
+np.savez_compressed(output_filename, **measures)
+
+
