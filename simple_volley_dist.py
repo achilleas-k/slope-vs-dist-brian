464c4ac9da6ad203d01502e6f71108181b40ecf6
Achilleas <achilleas.k@gmail.com>
2014-10-30 21:50:02 +0200

    Fixed spike raster

--- simple_volley_dist.py
+++ simple_volley_dist.py
@@ -35,7 +35,7 @@ mV = mvolt
 victor_purpura = sl.metrics.victor_purpura
 kreuz = sl.metrics.kreuz
 modulus = sl.metrics.modulus_metric
-npss = sl.tools.normalised_pre_spike_slopes
+npss = sl.tools.npss
 
 print("Creating NeuronGroup ...")
 netw = Network()
@@ -125,43 +125,43 @@ trace_mon.insert_spikes(output_mon, 40*mV)
 print("Simulation run finished! %i spikes fired." % (output_mon.nspikes))
 
 print("Calculating measures ...")
-outspikes = output_mon.spiketimes.values()
+outspikes = output_mon[0]
 inputspikes = input_mon.spiketimes.values()
 
 print("\tKreuz ...")
 # calculate interval kreuz from monitors
 kreuz_t, kreuz_d = kreuz.interval(inputspikes,
-                                  insert(outspikes[0], 0, 0), 1)
+                                  outspikes, 1)
 kreuz_t = [t for kt in kreuz_t for t in kt]
 kreuz_d = [k for kd in kreuz_d for k in kd]
 
 print("\tVictor-Purpura ...")
 # calculate victor-purpura from monitors
 vp_d = victor_purpura.interval(inputspikes,
-                               insert(outspikes[0], 0, 0),
+                               outspikes,
                                1/0.002)
 
 print("\tModulus ...")
 # calculate modulus metric
-modm = modulus.interval(inputspikes, insert(outspikes[0], 0, 0), 0, float(dura))
+modm = modulus.interval(inputspikes, outspikes, 0, float(dura))
 
 print("\tNPSS ...")
 # calculate NPSS
 norm_slopes = []
-for _idx in range(len(neurons)):
-    norm_slopes.append(npss(trace_mon[_idx], insert(outspikes[_idx], 0, 0),
-                            0*mV, Vth, tau, 2*ms))
+norm_slopes.append(npss(trace_mon[0], outspikes,
+                        0*mV, Vth, tau, 2*ms))
 
 
 print("Plotting ...")
 # plot them
 figure("Input spikes")
 subplot2grid((5,1), (0, 0), rowspan=4)
-raster_plot(input_mon, c='gray')
+for height, spiketrain in enumerate(input_mon.spiketimes.values()):
+    plot(spiketrain/ms, zeros_like(spiketrain)+height, '.', c='gray')
 ax2 = twinx()
 ax2.plot(array(ptimes)/ms, sigmas, color='black', linestyle='--', linewidth=1.0)
 subplot2grid((5,1), (4, 0))
-for out in outspikes[0]:
+for out in outspikes:
     plot([out/ms, out/ms], [0, Nin], 'k-', linewidth=1.0)
 axis(xmin=0, xmax=dura/ms)
 ax2.axis(xmin=0, xmax=dura/ms)
@@ -177,13 +177,13 @@ ylabel("$V$ (volt)")
 figure("Measures", figsize=(14.4, 9), dpi=100)
 textsize = 20
 #interval_midpoints = (outspikes[0]-diff(insert(outspikes[0], 0, 0))/2)*1000
-interval_midpoints = outspikes[0]*1000
+interval_midpoints = outspikes*1000
 subplot(511)
 #title("Packet widths")
 plot(multiply(ptimes, 1000), multiply(sigmas, 1000), 'k')
 ylabel("$\sigma$ (ms)", size=textsize)
 spike_height = max(sigmas)*1000
-for out in outspikes[0]:
+for out in outspikes:
     plot([out/ms, out/ms], [0, spike_height], color="gray", linestyle="--", linewidth=1.0)
 #for _idx, inp in input_mon.spiketimes.iteritems():
 #    scatter(inp*1000, zeros_like(inp)+spike_height*_idx/Nin, c='grey',
@@ -202,7 +202,7 @@ subplot(512)
 plot(interval_midpoints, norm_slopes[0], 'k')
 ylabel("$NPSS$", size=textsize)
 spike_height = max(norm_slopes[0])
-for out in outspikes[0]:
+for out in outspikes:
     plot([out/ms, out/ms], [0, spike_height], color="gray", linestyle="--", linewidth=1.0)
 xt, _ = xticks()
 xticks(xt, [])
@@ -215,10 +215,10 @@ text(text_x, text_y, "(e)", size=textsize)
 
 subplot(513)
 #title("Kreuz")
-plot(interval_midpoints, kreuz_d, 'k')
+plot(interval_midpoints[1:], kreuz_d, 'k')
 ylabel("$D_{S}$", size=textsize)
 spike_height = max(kreuz_d)
-for out in outspikes[0]:
+for out in outspikes:
     plot([out/ms, out/ms], [0, spike_height], color="gray", linestyle="--", linewidth=1.0)
 xt, _ = xticks()
 xticks(xt, [])
@@ -231,10 +231,10 @@ text(text_x, text_y, "(f)", size=textsize)
 
 subplot(514)
 #title("Victor-Purpura")
-plot(interval_midpoints, vp_d, 'k')
+plot(interval_midpoints[1:], vp_d, 'k')
 ylabel("$D_{V}$", size=textsize)
 spike_height = max(vp_d)
-for out in outspikes[0]:
+for out in outspikes:
     plot([out/ms, out/ms], [0, spike_height], color="gray", linestyle="--", linewidth=1.0)
 xt, _ = xticks()
 xticks(xt, [])
@@ -247,11 +247,11 @@ text(text_x, text_y, "(g)", size=textsize)
 
 subplot(515)
 #title("Modulus")
-plot(interval_midpoints, modm, 'k')
+plot(interval_midpoints[1:], modm, 'k')
 spike_height = max(modm)
 ylabel("$D_{m}$", size=textsize)
 xlabel("$t$ (ms)", size=textsize)
-for out in outspikes[0]:
+for out in outspikes:
     plot([out/ms, out/ms], [0, spike_height], color="gray", linestyle="--", linewidth=1.0)
 xticks(size=textsize)
 ylocs, ylabels = yticks()
