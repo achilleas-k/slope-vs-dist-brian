68ec443bcfcd35ab6f3bc3fc62717a67501022f6
Achilleas <achilleas.k@gmail.com>
2014-10-31 20:50:32 +0200

    Adjustable weight - calculate VPdist too

--- simpler_vd.py
+++ simpler_vd.py
@@ -32,7 +32,7 @@ def scaled_integral(membrane, spiketrain, dt=0.0001):
     return integrals
 
 
-duration = 30*second
+duration = 5*second
 print("Creating NeuronGroup ...")
 netw = brian.Network()
 Vth = 20*mV
@@ -47,16 +47,19 @@ netw.add(neurons)
 print("Creating inputs ...")
 base_spiketrain = np.arange(0.1*second, duration, 0.1*second)
 input_spiketimes = []
+n_input_trains = 10
 drift = 0*ms
 for spiketime in base_spiketrain:
-    drift += 0.1*ms
-    input_spiketimes.append((0, spiketime*second-drift))
-    input_spiketimes.append((1, spiketime*second))
+    drift -= 0.1*ms
+    for tridx in range(n_input_trains):
+        input_spiketimes.append((tridx, spiketime*second+tridx*drift))
 
-ingroup = brian.SpikeGeneratorGroup(2, input_spiketimes)
+ingroup = brian.SpikeGeneratorGroup(n_input_trains, input_spiketimes)
 netw.add(ingroup)
 print("Connecting inputs to neurons ...")
-connection = brian.Connection(ingroup, neurons, state='V', weight=19*mV)
+weight = Vth*1.5/n_input_trains
+print("Synaptic weight: %s" % str(weight))
+connection = brian.Connection(ingroup, neurons, state='V', weight=weight)
 netw.add(connection)
 
 # monitors
@@ -68,10 +71,21 @@ netw.add(tracemon, spikemon)
 print("Running simulation for %.2f seconds ..." % duration)
 netw.run(duration, report='stderr')
 tracemon.insert_spikes(spikemon, 40*mV)
-npss = sl.tools.npss(tracemon[0], spikemon[0], 0*mV, Vth, tau, 2*ms)
 print("Simulation done!")
+input_spiketrains = []
+for _ in range(n_input_trains):
+    input_spiketrains.append([])
+for spike in input_spiketimes:
+    input_spiketrains[spike[0]].append(spike[1])
+print("Calculating NPSS ...")
+npss = sl.tools.npss(tracemon[0], spikemon[0], 0*mV, Vth, tau, 2*ms)
+print("Calculating pairwise Kreuz distance (interval mode) ...")
+krint = sl.metrics.kreuz.interval(input_spiketrains, spikemon[0], mp=True)
+print("Calculating pairwise VP distance (interval mode) ...")
+vpdist = sl.metrics.victor_purpura.interval(input_spiketrains, spikemon[0],
+                                            1/(2*ms))
 
-print("Plots ...")
+print("Reticulating splines ...")
 dt = brian.defaultclock.dt
 winstarts = spikemon[0]-(2*ms)
 winstarts_idx = (winstarts/dt).astype('int')
@@ -82,18 +96,19 @@ plt.plot(tracemon.times[winstarts_idx], tracemon[0][winstarts_idx],
          'r.', markersize=10)
 
 plt.figure()
-plt.title("Kreuz and NPSS")
-input_spiketrains = [[], []]
-for spike in input_spiketimes:
-    input_spiketrains[spike[0]].append(spike[1])
-krint = sl.metrics.kreuz.interval(input_spiketrains,
-                                  spikemon[0], mp=False)
-plt.plot(krint[0], krint[1])
-plt.plot(spikemon[0], npss)
+plt.title("All measures")
+plt.plot(spikemon[0], npss, label="NPSS")
+plt.plot(krint[0], krint[1], label="Kreuz")
+plt.plot(spikemon[0], vpdist, label="VP")
+plt.legend()
 
 plt.figure()
 plt.title("Kreuz vs NPSS")
-plt.scatter(krint[0], npss[1:])
+plt.scatter(krint[1], npss[1:])
+
+plt.figure()
+plt.title("VP vs NPSS")
+plt.scatter(vpdist, npss[1:])
 
 integrals = interval_integral(tracemon[0], spikemon[0])
 scintegrals = scaled_integral(tracemon[0], spikemon[0])
