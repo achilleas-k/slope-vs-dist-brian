349ed4109bcf20280b9f6466de80f298f32ce8d4
Achilleas <achilleas.k@gmail.com>
2014-11-14 03:16:33 +0200

    Run using multivariate and save all samples

--- grid_kreuz.py
+++ grid_kreuz.py
@@ -5,17 +5,18 @@ import numpy as np
 import multiprocessing as mp
 
 
-def call_interval_kr(record):
+def call_mulitvar_kr(record):
     id = record["id"]
     inspikes = record["inspikes"]
-    outspikes = record["outspikes"]
-    dist_kr = np.mean(kr.interval(inspikes, outspikes, mp=False))
+    start, end = 0, max([max(insp) for insp in inspikes])
+    samples = end*0.001  # 1 sample per millisecond
+    dist_kr = kr.multivariate(inspikes, start, end, samples)
     return {"id": id, "kr": dist_kr}
 
 spikes = np.load("2014-01-31_spikes.npz")["data"]
 _num_items = len(spikes)
 pool = mp.Pool()
-kreuz_iter = pool.imap(call_interval_kr, spikes)
+kreuz_iter = pool.imap(call_mulitvar_kr, spikes)
 print("Starting calculation of kreuz ...")
 dist_kr = []
 for idx, kr_item in enumerate(kreuz_iter, 1):
