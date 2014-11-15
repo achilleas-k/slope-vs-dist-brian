05d719c072f91358e1ddf0e4168ddf940ecb111c
Achilleas <achilleas.k@gmail.com>
2014-11-15 19:46:06 +0200

    Run simulations and generate NPSS and Kreuz multivariate results

--- npss_kreuz_standalone.py
+++ npss_kreuz_standalone.py
@@ -3,14 +3,11 @@ Monte-Carlo version of DataGenTaskFarm.py
 The simulation parameters are specified by range bounds (min, max) and number
 of simulations.
 """
+from __future__ import print_function, division
 from brian import *
 from brian.tools.taskfarm import *
 from brian.tools.datamanager import *
-import warnings
-from spike_distance import mean_pairwise_distance
 import spikerlib as sl
-from spike_distance_kreuz import multivariate_spike_distance
-import itertools as itt
 import gc
 from time import time
 import os
@@ -19,48 +16,10 @@ from datetime import datetime
 
 defaultclock.dt = dt = 0.1*ms
 duration = 5*second
-
-warnings.simplefilter("always")
 slope_w = 2*msecond
-dcost = float(2/slope_w)
 
 
-def interval_VP(inputspikes, outputspikes, cost, dt=0.1*ms):
-    dt = float(dt)
-    vpdists = []
-    for prv, nxt in zip(outputspikes[:-1], outputspikes[1:]):
-        interval_inputs = []
-        for insp in inputspikes:
-            interval_inputs.append(insp[(prv < insp) & (insp < nxt+dt)])
-        vpd = mean_pairwise_distance(interval_inputs, cost)
-        vpdists.append(vpd)
-    return vpdists
-
-
-def interval_Kr(inputspikes, outputspikes, dt=0.1*ms):
-    dt = float(dt)
-    krdists = []
-    for prv, nxt in zip(outputspikes[:-1], outputspikes[1:]):
-        krd = multivariate_spike_distance(inputspikes, prv, nxt+dt, 1)
-        krdists.append(krd[1])
-    return krdists
-
-
-def interval_corr(inputspikes, outputspikes, b=0.1*ms, duration=None):
-    b = float(b)
-    corrs = []
-    for prv, nxt in zip(outputspikes[:-1], outputspikes[1:]):
-        interval_inputs = []
-        for insp in inputspikes:
-            interval_spikes = insp[(prv < insp) & (insp <= nxt)]-prv
-            if len(interval_spikes):
-                interval_inputs.append(interval_spikes)
-        corrs_i = mean(corrcoef_spiketrains(interval_inputs, b, duration))
-        corrs.append(corrs_i)
-    return corrs
-
-
-def lifsim(sync, sgm, N_in, inrate, weight):
+def lifsim(sync, sgm, n_in, inrate, weight):
     clear(True)
     gc.collect()
     reinit_default_clock()
@@ -100,22 +59,19 @@ def lifsim(sync, sgm, N_in, inrate, weight):
     if outmon.nspikes < 2:
         return
     vmon.insert_spikes(outmon, Vth)
-    input_spiketrains = syncmon.spiketimes.values()
-    input_spiketrains += randmon.spiketimes.values()
-    input_spiketrains = array(input_spiketrains)
-
-    kr_dists = sl.metrics.kreuz.multivariate(input_spiketrains, 0*second,
-                                             duration, int(duration/dt))
+    input_spiketrains = inputmon.spiketimes.values()
+    kr_times, kr_dists = sl.metrics.kreuz.multivariate(input_spiketrains,
+                                                       0*second, duration,
+                                                       int(duration/dt))
+    npss = sl.tools.npss(vmon[0], outmon[0], 0*mV, Vth, tau, slope_w)
 
     results = {'id': seed,
                'mem': vmon[0],
                'outspikes': outmon[0],
                'inspikes': input_spiketrains,
-               'slopes': slopes,
                'npss': npss,
-               'vp_dists': vp_dists,
-               'kr_dists': kr_dists,
-               'correlations': corrs}
+               'kr_times': kr_times,
+               'kr_dists': kr_dists}
     np.savez("%i.npz" % seed, config=config, results=results)
     return
 
@@ -125,7 +81,7 @@ if __name__=='__main__':
     today = datetime.now()
     today_str = "{}{}{}".format(today.year, today.month, today.day)
     data = DataManager(today_str)
-    num_sims = 100
+    num_sims = 2
     num_inputs = 50+randint(150, size=num_sims)  # 50, 200
     input_frequencies = 10+np.round(90*random(num_sims), 0)  # 10, 100
     input_weights = np.round(5e-4*random(num_sims)+1e-4, 5)  # 1e-4, 5e-4
