7d6adac07a62be922ef420ea13157a28f58cef7e
Achilleas <achilleas.k@gmail.com>
2014-12-03 22:40:37 +0200

    Make plot and curves

--- for_paper/makeplots.py
+++ for_paper/makeplots.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import spikerlib as sl
+from scipy.optimize import curve_fit
 
 def get_mnpss(results):
     return np.array([np.mean(r["npss"]) for r in results])
@@ -12,7 +13,18 @@ def get_kreuz(results):
 def get_param(configs, paramkey):
     return np.array([c[paramkey] for c in configs])
 
+def cf3(x, a, b, c):
+    return np.polyval([a, b, c], x)
+
+def cf4(x, a, b, c, d):
+    return np.polyval([a, b, c, d], x)
+
+def cf5(x, a, b, c, d, e):
+    return np.polyval([a, b, c, d, e], x)
+
 plt.rcParams['image.cmap'] = 'gray'
+
+print("Loading data...")
 data_nojitter = np.load("../2014-11-16-npz-nojitter/results.npz")
 data_jitter = np.load("../2014-11-17-npz/results.npz")
 
@@ -20,31 +32,48 @@ configs_nj = data_nojitter["configs"]
 results_nj = data_nojitter["results"]
 configs_j = data_jitter["configs"]
 results_j = data_jitter["results"]
-
-fig = plt.figure("NPSS vs SPIKE-distance", dpi=100, figsize=(8,6))
 mnpss = np.append(get_mnpss(results_nj), get_mnpss(results_j))
 kreuz = np.append(get_kreuz(results_nj), get_kreuz(results_j))
 jitters = np.append(get_param(configs_nj, "sigma"),
                     get_param(configs_j, "sigma"))
+
+print("Fitting curves...")
+curvefunc = cf5
+popt, pcov = curve_fit(curvefunc, mnpss, kreuz)
+curvepts = curvefunc(mnpss, *popt)
+
+njidx = jitters == 0
+poptnj, pcov = curve_fit(curvefunc, mnpss[njidx], kreuz[njidx])
+njcurvepts = curvefunc(mnpss[njidx], *poptnj)
+
+pjidx = jitters > 0
+poptpj, pcov = curve_fit(curvefunc, mnpss[pjidx], kreuz[pjidx])
+pjcurvepts = curvefunc(mnpss[pjidx], *poptpj)
+
+print("Making plots...")
+fig = plt.figure("NPSS vs SPIKE-distance", dpi=100, figsize=(8,6))
 ### All data points
 plt.subplot2grid((11,11), (0,0), rowspan=4, colspan=10)
 allpts = plt.scatter(mnpss, kreuz, c=jitters*1000)
-plt.xlabel(r"$\bar{M}$")
+plt.plot(mnpss, curvepts, "r*")
+plt.xlabel(r"$\overline{M}$")
 plt.ylabel(r"$D_S$")
 plt.axis(xmin=0, xmax=1, ymin=0)
 
 ### Split jitter from no-jitter
-njidx = jitters == 0
 plt.subplot2grid((11,11), (6,0), rowspan=4, colspan=4)
 njpts = plt.scatter(mnpss[njidx], kreuz[njidx], c=jitters[njidx]*1000)
-plt.xlabel(r"$\bar{M}$")
+plt.plot(mnpss, curvepts, "r*")
+plt.plot(mnpss[njidx], njcurvepts, "m*")
+plt.xlabel(r"$\overline{M}$")
 plt.ylabel(r"$D_S$")
 plt.axis(xmin=0, xmax=1, ymin=0)
 
-pjidx = jitters > 0
 plt.subplot2grid((11,11), (6,6), rowspan=4, colspan=4)
 plt.scatter(mnpss[pjidx], kreuz[pjidx], c=jitters[pjidx]*1000)
-plt.xlabel(r"$\bar{M}$")
+plt.plot(mnpss, curvepts, "r*")
+plt.plot(mnpss[pjidx], pjcurvepts, "m*")
+plt.xlabel(r"$\overline{M}$")
 plt.ylabel(r"$D_S$")
 plt.axis(xmin=0, xmax=1, ymin=0)
 
@@ -54,3 +83,7 @@ cbar.set_label(r"$\sigma_{in}$")
 
 plt.subplots_adjust(wspace=0.2, hspace=0.2)
 plt.savefig("npss_v_dist.eps")
+
+print("Fitted curve coefficients: {}".format(", ".join(str(p) for p in popt)))
+
+
