a5778e804b31b9517abc1084af63d52a6ab44c77
Achilleas <achilleas.k@gmail.com>
2014-12-04 17:15:08 +0200

    colours

--- for_paper/makeplots.py
+++ for_paper/makeplots.py
@@ -1,7 +1,5 @@
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-import spikerlib as sl
 from scipy.optimize import curve_fit
 
 def get_mnpss(results):
@@ -22,6 +20,9 @@ def cf4(x, a, b, c, d):
 def cf5(x, a, b, c, d, e):
     return np.polyval([a, b, c, d, e], x)
 
+def cf9(x, a, b, c, d, e, f, g, h, i):
+    return np.polyval([a, b, c, d, e, f, g, h, i], x)
+
 plt.rcParams['image.cmap'] = 'gray'
 
 print("Loading data...")
@@ -37,8 +38,13 @@ kreuz = np.append(get_kreuz(results_nj), get_kreuz(results_j))
 jitters = np.append(get_param(configs_nj, "sigma"),
                     get_param(configs_j, "sigma"))
 
+sorted_idx = np.argsort(mnpss)
+mnpss = mnpss[sorted_idx]
+kreuz = kreuz[sorted_idx]
+jitters = jitters[sorted_idx]
+
 print("Fitting curves...")
-curvefunc = cf5
+curvefunc = cf9
 popt, pcov = curve_fit(curvefunc, mnpss, kreuz)
 curvepts = curvefunc(mnpss, *popt)
 
@@ -50,12 +56,14 @@ pjidx = jitters > 0
 poptpj, pcov = curve_fit(curvefunc, mnpss[pjidx], kreuz[pjidx])
 pjcurvepts = curvefunc(mnpss[pjidx], *poptpj)
 
+
+
 print("Making plots...")
 fig = plt.figure("NPSS vs SPIKE-distance", dpi=100, figsize=(8,6))
 ### All data points
 plt.subplot2grid((11,11), (0,0), rowspan=4, colspan=10)
 allpts = plt.scatter(mnpss, kreuz, c=jitters*1000)
-plt.plot(mnpss, curvepts, "r*")
+plt.plot(mnpss, curvepts, color="black", linestyle="-", linewidth=5, alpha=0.5)
 plt.xlabel(r"$\overline{M}$")
 plt.ylabel(r"$D_S$")
 plt.axis(xmin=0, xmax=1, ymin=0)
@@ -63,16 +71,18 @@ plt.axis(xmin=0, xmax=1, ymin=0)
 ### Split jitter from no-jitter
 plt.subplot2grid((11,11), (6,0), rowspan=4, colspan=4)
 njpts = plt.scatter(mnpss[njidx], kreuz[njidx], c=jitters[njidx]*1000)
-plt.plot(mnpss, curvepts, "r*")
-plt.plot(mnpss[njidx], njcurvepts, "m*")
+plt.plot(mnpss, curvepts, color="black", linestyle="-", linewidth=5, alpha=0.5)
+plt.plot(mnpss[njidx], njcurvepts, color="magenta", linestyle="-",
+         linewidth=3, alpha=0.6)
 plt.xlabel(r"$\overline{M}$")
 plt.ylabel(r"$D_S$")
 plt.axis(xmin=0, xmax=1, ymin=0)
 
 plt.subplot2grid((11,11), (6,6), rowspan=4, colspan=4)
 plt.scatter(mnpss[pjidx], kreuz[pjidx], c=jitters[pjidx]*1000)
-plt.plot(mnpss, curvepts, "r*")
-plt.plot(mnpss[pjidx], pjcurvepts, "m*")
+plt.plot(mnpss, curvepts, color="black", linestyle="-", linewidth=5, alpha=0.5)
+plt.plot(mnpss[pjidx], pjcurvepts, color="magenta", linestyle="-",
+         linewidth=3, alpha=0.6)
 plt.xlabel(r"$\overline{M}$")
 plt.ylabel(r"$D_S$")
 plt.axis(xmin=0, xmax=1, ymin=0)
@@ -82,7 +92,8 @@ cbar = plt.colorbar(cax=cax)
 cbar.set_label(r"$\sigma_{in}$")
 
 plt.subplots_adjust(wspace=0.2, hspace=0.2)
-plt.savefig("npss_v_dist.eps")
+plt.savefig("npss_v_dist.pdf")
+plt.savefig("npss_v_dist.png")
 
 print("Fitted curve coefficients: {}".format(", ".join(str(p) for p in popt)))
 
