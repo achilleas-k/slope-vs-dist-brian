62e7ad1c7687ecc41fe752496449040b07fceadf
Achilleas <achilleas.k@gmail.com>
2014-12-05 16:24:26 +0200

    Error histograms

--- for_paper/makeplots.py
+++ for_paper/makeplots.py
@@ -1,3 +1,4 @@
+from __future__ import division, print_function
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
@@ -27,6 +28,12 @@ def cf9(x, a, b, c, d, e, f, g, h, i):
 def calcerrors(x, y, coef):
     target = np.polyval(coef, x)
     return abs(target-y)
+
+def clip(x, min, max):
+    x[x < min] = min
+    x[x > max] = max
+    return x
+
 
 plt.rcParams['image.cmap'] = 'gray'
 
@@ -63,6 +70,8 @@ mnpss = mnpss[sorted_idx]
 kreuz = kreuz[sorted_idx]
 jitters = jitters[sorted_idx]
 
+drive = (inrates*numin*weights*0.01).astype("float")
+
 print("Fitting curves...")
 curvefunc = cf5
 popt, pcov = curve_fit(curvefunc, mnpss, kreuz)
@@ -84,8 +93,11 @@ print("Total:          {}".format(len(mnpss)))
 print("Making plots...")
 fig = plt.figure("NPSS vs SPIKE-distance", dpi=100, figsize=(8,6))
 ### All data points
+vmax = 1.0
+colour = clip(jitters*10000, 0, vmax)
 plt.subplot2grid((11,11), (0,0), rowspan=4, colspan=10)
-allpts = plt.scatter(mnpss, kreuz, c=jitters*1000)
+plt.title("(a)"+" "*110)
+allpts = plt.scatter(mnpss, kreuz, vmin=0, vmax=vmax, c=colour)# c=jitters*1000)
 plt.plot(mnpss, curvepts, color="black", linestyle="-", linewidth=5, alpha=0.5)
 plt.xlabel(r"$\overline{M}$")
 plt.ylabel(r"$D_S$")
@@ -93,25 +105,27 @@ plt.axis(xmin=0, xmax=1, ymin=0)
 
 ### Split jitter from no-jitter
 plt.subplot2grid((11,11), (6,0), rowspan=4, colspan=4)
-njpts = plt.scatter(mnpss[njidx], kreuz[njidx], c=jitters[njidx]*1000)
-plt.plot(mnpss, curvepts, color="grey", linestyle="-", linewidth=5, alpha=0.5)
-plt.plot(mnpss[njidx], njcurvepts, color="black", linestyle="-",
-         linewidth=3, alpha=0.6)
+plt.title("(b)"+" "*55)
+njpts = plt.scatter(mnpss[njidx], kreuz[njidx], vmin=0, vmax=vmax, c=colour[njidx])# c=jitters[njidx]*1000)
+plt.plot(mnpss, curvepts, color="black", linestyle="-", linewidth=5, alpha=0.5)
+#plt.plot(mnpss[njidx], njcurvepts, color="black", linestyle="-",
+#         linewidth=3, alpha=0.6)
 plt.xlabel(r"$\overline{M}$")
 plt.ylabel(r"$D_S$")
 plt.axis(xmin=0, xmax=1, ymin=0)
 
 plt.subplot2grid((11,11), (6,6), rowspan=4, colspan=4)
-plt.scatter(mnpss[pjidx], kreuz[pjidx], c=jitters[pjidx]*1000)
-plt.plot(mnpss, curvepts, color="grey", linestyle="-", linewidth=5, alpha=0.5)
-plt.plot(mnpss[pjidx], pjcurvepts, color="black", linestyle="-",
-         linewidth=3, alpha=0.6)
+plt.title("(c)"+" "*55)
+plt.scatter(mnpss[pjidx], kreuz[pjidx], vmin=0, vmax=vmax, c=colour[pjidx])# c=jitters[pjidx]*1000)
+plt.plot(mnpss, curvepts, color="black", linestyle="-", linewidth=5, alpha=0.5)
+#plt.plot(mnpss[pjidx], pjcurvepts, color="black", linestyle="-",
+#         linewidth=3, alpha=0.6)
 plt.xlabel(r"$\overline{M}$")
 plt.ylabel(r"$D_S$")
 plt.axis(xmin=0, xmax=1, ymin=0)
 
 cax = fig.add_axes([0.9, 0.15, 0.03, 0.75])
-cbar = plt.colorbar(cax=cax)
+cbar = plt.colorbar(allpts, cax=cax)
 cbar.set_label(r"$\sigma_{in}$ (ms)")
 
 plt.subplots_adjust(wspace=0.2, hspace=0.2)
@@ -120,14 +134,81 @@ plt.savefig("npss_v_dist.png")
 
 print("Fitted curve coefficients: {}".format(", ".join(str(p) for p in popt)))
 
-print("Calculating deviations...")
+print("Calculating plotting deviations...")
+allerrors = calcerrors(mnpss, kreuz, popt)
 njerrors = calcerrors(mnpss[njidx], kreuz[njidx], popt)
 pjerrors = calcerrors(mnpss[pjidx], kreuz[pjidx], popt)
-plt.figure("Deviations")
-plt.scatter(synchs[pjidx], pjerrors)
-#cbar = plt.colorbar()
-#cbar.set_label(r"$S_{in}$")
+plt.figure("Jitter vs Errors")
+allerrs = plt.scatter(jitters, allerrors, c=drive)
+cbar = plt.colorbar(allerrs)
+cbar.set_label(r"$\langle V \rangle$")
 plt.xlabel(r"$\sigma_{in}$ (ms)")
 plt.ylabel("Abs. error")
-plt.savefig("deviations.pdf")
-plt.savefig("deviations.png")
+plt.savefig("jitters_v_errors.pdf")
+plt.savefig("jitters_v_errors.png")
+
+print("Plotting error histograms (with jitter)...")
+plt.figure("Histograms (jitter)")
+#plt.hist(njerrors, color="black", normed=False, alpha=1.0)
+#plt.hist(pjerrors, color="grey",  normed=False, alpha=0.5)
+njhy, njhx = np.histogram(njerrors)
+pjhy, pjhx = np.histogram(pjerrors)
+plt.plot(njhx[:-1], njhy/len(allerrors), c="black",
+         label="$\sigma_{in} = 0$ ms")
+plt.plot(pjhx[:-1], pjhy/len(allerrors), c="grey",
+         label="$\sigma_{in} > 0$ ms")
+allhy, allhx = np.histogram(allerrors)
+plt.plot(allhx[:-1], allhy/len(allerrors), c="black", linestyle="--",
+         label="$\sigma_{in} \geq 0$ ms")
+plt.legend(loc="best")
+plt.savefig("error_hist_jitter.pdf")
+plt.savefig("error_hist_jitter.png")
+
+print("Average absolute errors")
+print("Without jitter: {}".format(np.mean(njerrors)))
+print("With jitter:    {}".format(np.mean(pjerrors)))
+print("All:            {}".format(np.mean(allerrors)))
+
+print("Plotting results with drive...")
+fig = plt.figure("NPSS vs SPIKE-distance with drive", dpi=100, figsize=(8,6))
+idx = drive > 0.02
+allpts = plt.scatter(mnpss[idx], kreuz[idx], c=drive[idx])
+plt.plot(mnpss, curvepts, color="black", linestyle="-", linewidth=5, alpha=0.5)
+plt.xlabel(r"$\overline{M}$")
+plt.ylabel(r"$D_S$")
+plt.axis(xmin=0, xmax=1, ymin=0)
+cbar = plt.colorbar(allpts)
+cbar.set_label(r"$\langle V \rangle$")
+plt.savefig("npss_v_dist_drive.pdf")
+plt.savefig("npss_v_dist_drive.png")
+
+plt.figure("Drive vs Errors")
+plt.scatter(drive, allerrors, c="black")
+plt.xlabel(r"$\langle V \rangle$")
+plt.ylabel("Abs. error")
+plt.savefig("drive_v_errors.pdf")
+plt.savefig("drive_v_errors.png")
+
+print("Plotting error histograms (with drive)...")
+plt.figure("Histograms (drive)")
+hdhy, hdhx = np.histogram(allerrors[drive>=0.030])
+gdhy, gdhx = np.histogram(allerrors[(drive>=0.015) & (drive < 0.030)])
+ldhy, ldhx = np.histogram(allerrors[drive<0.015])
+plt.plot(hdhx[:-1], hdhy/len(allerrors), c="grey", linestyle="--",
+         label=r"$\langle V \rangle \geq 2V_{th}$")
+plt.plot(gdhx[:-1], gdhy/len(allerrors), c="black",
+         label=r"$\langle V \rangle \geq V_{th}$")
+plt.plot(ldhx[:-1], ldhy/len(allerrors), c="grey",
+         label=r"$\langle V \rangle < V_{th}$")
+allhy, allhx = np.histogram(allerrors)
+plt.plot(allhx[:-1], allhy/len(allerrors), c="black", linestyle="--",
+         label="All data")
+plt.legend(loc="best")
+plt.savefig("error_hist_drive.pdf")
+plt.savefig("error_hist_drive.png")
+
+print("Average absolute errors")
+print("Without jitter: {}".format(np.mean(njerrors)))
+print("With jitter:    {}".format(np.mean(pjerrors)))
+print("All:            {}".format(np.mean(allerrors)))
+print("Done")
