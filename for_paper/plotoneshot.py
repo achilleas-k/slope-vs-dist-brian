165b5e480c799a115e22f0ca906b67f295dcd68e
Achilleas <achilleas.k@gmail.com>
2014-12-17 00:55:23 +0200

    Wonderful

--- for_paper/plotoneshot.py
+++ for_paper/plotoneshot.py
@@ -2,8 +2,9 @@
 Pick fixed values to demonstrate different cases and smooth lines.
 """
 from __future__ import division, print_function
-from brian import *
 import numpy as np
+import matplotlib.pyplot as plt
+from brian import mV, ms
 import sys
 from glob import glob
 
@@ -80,18 +81,49 @@ if __name__=='__main__':
         count += 1
         print("{}/{} done".format(count, len(npzfiles)))
     plt.clf()
-    allmnpss = array(allmnpss)
-    allkrdst = array(allkrdst)
-    alldrive = array(alldrive)
-    allpeaks = array(allpeaks)
-    allsigma = array(allsigma)
-    sanedrive = (16 <= alldrive) & (alldrive <= 30)
-    sanepeaks = (16 <= allpeaks) & (allpeaks <= 30)
-    idx = sanedrive & sanepeaks
-    plt.scatter(allmnpss[idx], allkrdst[idx], c=allsigma[idx]*1000)
+    mnpss = np.array(allmnpss)
+    kreuz = np.array(allkrdst)
+    drive = np.array(alldrive)
+    peaks = np.array(allpeaks)
+    jitters = np.array(allsigma)
+    njidx = jitters == 0
+    pjidx = jitters > 0
+
+    sanedrvidx =  (15 < drive) & (drive <= 30)
+    sanepeakidx = (15 < peaks) & (peaks <= 30)
+    saneidx = sanedrvidx & sanepeakidx
+
+    fig = plt.figure("NPSS vs SPIKE-distance with jitter", dpi=100, figsize=(8,6))
+    colour = jitters*1000
+    vmax = max(colour)
+    plt.subplot2grid((11,11), (0,0), rowspan=4, colspan=10)
+    plt.title("(a)")
+    idx = saneidx
+    allpts = plt.scatter(mnpss[idx], kreuz[idx], vmin=0, vmax=vmax, c=colour[idx])
     plt.xlabel(r"$\overline{M}$")
-    plt.ylabel(r"$S_{m}$")
-    plt.colorbar()
-    figname = "all_npss_kreuz.png"
-    plt.savefig(figname)
-    print("Saved figure {}".format(figname))
+    plt.ylabel(r"$D_S$")
+    plt.axis(xmin=0, xmax=1, ymin=0)
+
+    plt.subplot2grid((11,11), (6,0), rowspan=4, colspan=4)
+    plt.title("(b)")
+    idx = saneidx & njidx
+    njpts = plt.scatter(mnpss[idx], kreuz[idx], vmin=0, vmax=vmax, c=colour[idx])
+    plt.xlabel(r"$\overline{M}$")
+    plt.ylabel(r"$D_S$")
+    plt.axis(xmin=0, xmax=1, ymin=0)
+
+    plt.subplot2grid((11,11), (6,6), rowspan=4, colspan=4)
+    plt.title("(c)")
+    idx = saneidx & pjidx
+    plt.scatter(mnpss[idx], kreuz[idx], vmin=0, vmax=vmax, c=colour[idx])
+    plt.xlabel(r"$\overline{M}$")
+    plt.ylabel(r"$D_S$")
+    plt.axis(xmin=0, xmax=1, ymin=0)
+
+    cax = fig.add_axes([0.85, 0.15, 0.03, 0.75])
+    cbar = plt.colorbar(allpts, cax=cax)
+    cbar.set_label(r"$\sigma_{in}$ (ms)")
+
+    plt.subplots_adjust(wspace=0.2, hspace=0.2)
+    plt.savefig("npss_v_dist_jitter.pdf")
+    plt.savefig("npss_v_dist_jitter.png")
